964f7a6792acd742b9511392990b324ef4d2ebab
Ryan Mehta <ryan.mehta@gmail.com>
2020-03-27 10:22:19 -0700

    init

---
+++ Reporter.js
@@ -0,0 +1,22 @@
+const { execSync } = require('child_process');
+const request = require('superagent');
+
+module.exports = class Reporter {
+  constructor(config, options) {
+    this.config = config;
+    this.options = options;
+  }
+
+  onRunComplete(contexts, results) {
+    const name = execSync('git config --get user.name', { encoding: 'utf8' }).trim();
+    const email = execSync('git config --get user.email', { encoding: 'utf8' }).trim();
+    request
+      .post('http://localhost:7890/api/v1/reports')
+      .send({
+        ...results,
+        name,
+        email
+      })
+      .end();
+  }
+};
