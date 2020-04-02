2ae223332d43b894254a5ce77947de15b2eff78f
Ryan Mehta <ryan.mehta@gmail.com>
2020-04-02 11:52:10 -0700

    stuff

--- twilio.js
+++ twilio.js
@@ -6,7 +6,8 @@ const token = process.env.TWILIO_TOKEN;
 const twilio = new Twilio(sid, token);
 
 const defaultSong = 'https://t4.bcbits.com/stream/b7e07f1271193e70864115e5917d3963/mp3-128/1068935279?p=0&ts=1585929229&t=b6d1290fe8d7f498ccf04f66d22853950b70c22a&token=1585929229_21ebfa57053ebe4d75fc1272a18c33bb0b6b1c99';
-const calls = {}
+const calls = {};
+const jukeBox = {};
 
 const callZoom = (meetingId, twiml) => {
   return twilio.calls.create({
@@ -16,7 +17,7 @@ const callZoom = (meetingId, twiml) => {
     twiml
   })
     .then(call => {
-      calls[meetingId] = { sid: call.sid, songs: [] };
+      calls[meetingId] = call.sid;
       return call;
     })
 };
@@ -26,7 +27,7 @@ const endZoom = meetingId => {
   hangup.hangup();
 
   return twilio.calls
-    .get(calls[meetingId].sid)
+    .get(calls[meetingId])
     .update({
       twiml: hangup.toString()
     });
@@ -34,17 +35,18 @@ const endZoom = meetingId => {
 
 const zoomOrchestrate = meetingId => {
   const play = new VoiceResponse();
-  play.play(calls[meetingId].songs.pop() || defaultSong);
+  play.play((jukeBox[meetingId] || []).pop() || defaultSong);
   play.redirect(`https://jest-test-rss.herokuapp.com/next/${meetingId}`);
 
   return play.toString();
 }
 
 const addSong = (meetingId, song) => {
-  calls[meetingId].songs.unshift(song);
+  if(!jukeBox[meetingId]) jukeBox[meetingId] = [];
+  jukeBox[meetingId].unshift(song);
 }
 
-const getSongs = meetingId => calls[meetingId].songs;
+const getSongs = meetingId => jukeBox[meetingId] || [];
 
 module.exports = {
   callZoom,
