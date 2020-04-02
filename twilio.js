6f4508d5acfac522fe6018eb6e19968e1fb8da3c
Ryan Mehta <ryan.mehta@gmail.com>
2020-04-02 15:32:37 -0700

    add events route

--- twilio.js
+++ twilio.js
@@ -5,7 +5,7 @@ const token = process.env.TWILIO_TOKEN;
 
 const twilio = new Twilio(sid, token);
 
-const defaultSong = 'https://t4.bcbits.com/stream/b7e07f1271193e70864115e5917d3963/mp3-128/1068935279?p=0&ts=1585929229&t=b6d1290fe8d7f498ccf04f66d22853950b70c22a&token=1585929229_21ebfa57053ebe4d75fc1272a18c33bb0b6b1c99';
+const defaultSong = 'https://jest-test-rss.herokuapp.com/child.mp3';
 const calls = {};
 const jukeBox = {};
 
