c2bf3be6c8df91170e44e8f6a8962cdbb04edac9
Ryan Mehta <ryan.mehta@gmail.com>
2020-04-02 11:47:27 -0700

    stuff

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const app = express();
-const { callZoom, zoomOrchestrate, endZoom } = require('./twilio');
+const {
+  callZoom,
+  zoomOrchestrate,
+  endZoom,
+  addSong,
+  getSongs
+} = require('./twilio');
 
 app.use(express.json());
 app.use(express.static('./public'))
 
-const defaultSong = 'https://t4.bcbits.com/stream/b7e07f1271193e70864115e5917d3963/mp3-128/1068935279?p=0&ts=1585929229&t=b6d1290fe8d7f498ccf04f66d22853950b70c22a&token=1585929229_21ebfa57053ebe4d75fc1272a18c33bb0b6b1c99';
-const songs = [];
 
 app.post('/start', (req, res) => {
-  callZoom(req.body.meetingId, zoomOrchestrate(songs.pop() || defaultSong))
+  callZoom(req.body.meetingId)
     .then(call => res.send(call));
 });
 
@@ -18,19 +22,19 @@ app.post('/stop', (req, res) => {
     .then(call => res.send(call));
 });
 
-app.post('/songs', (req, res) => {
-  songs.unshift(req.body.song);
+app.post('/songs/:meetingId', (req, res) => {
+  addSong(req.params.meetingId, req.body.song);
   res.status(204).end();
 });
 
-app.get('/songs', (req, res) => {
-  res.send(songs);
+app.get('/songs/:meetingId', (req, res) => {
+  res.send(getSongs(req.params.meetingId));
 });
 
-app.post('/next', (req, res) => {
+app.post('/next/:meetingId', (req, res) => {
   res
     .contentType('text/xml')
-    .send(zoomOrchestrate(songs.pop() || defaultSong));
+    .send(zoomOrchestrate(req.params.meetingId));
 });
 
 app.listen(process.env.PORT || 7890);
--- twilio.js
+++ twilio.js
@@ -5,6 +5,7 @@ const token = process.env.TWILIO_TOKEN;
 
 const twilio = new Twilio(sid, token);
 
+const defaultSong = 'https://t4.bcbits.com/stream/b7e07f1271193e70864115e5917d3963/mp3-128/1068935279?p=0&ts=1585929229&t=b6d1290fe8d7f498ccf04f66d22853950b70c22a&token=1585929229_21ebfa57053ebe4d75fc1272a18c33bb0b6b1c99';
 const calls = {}
 
 const callZoom = (meetingId, twiml) => {
@@ -15,7 +16,7 @@ const callZoom = (meetingId, twiml) => {
     twiml
   })
     .then(call => {
-      calls[meetingId] = call.sid;
+      calls[meetingId] = { sid: call.sid, songs: [] };
       return call;
     })
 };
@@ -25,22 +26,30 @@ const endZoom = meetingId => {
   hangup.hangup();
 
   return twilio.calls
-    .get(calls[meetingId])
+    .get(calls[meetingId].sid)
     .update({
       twiml: hangup.toString()
     });
 }
 
-const zoomOrchestrate = song => {
+const zoomOrchestrate = meetingId => {
   const play = new VoiceResponse();
-  play.play(song);
-  play.redirect('https://jest-test-rss.herokuapp.com/next');
+  play.play(calls[meetingId].songs.pop() || defaultSong);
+  play.redirect(`https://jest-test-rss.herokuapp.com/next/${meetingId}`);
 
   return play.toString();
 }
 
+const addSong = (meetingId, song) => {
+  calls[meetingId].songs.unshift(song);
+}
+
+const getSongs = meetingId => calls[meetingId].songs;
+
 module.exports = {
   callZoom,
   endZoom,
+  addSong,
+  getSongs,
   zoomOrchestrate
 }
